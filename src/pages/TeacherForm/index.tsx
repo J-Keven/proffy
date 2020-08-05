d82a4d1cd3408cf8765577e8d4490a3f8f62f00c
Keven <jhonasnunes425@gmail.com>
2020-08-05 10:38:45 -0300

    add folder backend from aplication

--- src/pages/TeacherForm/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from  'react'
-
-export default function TeacherFomr() {
-
-  return <h1>teacher form</h1>
-}
--- src/pages/TeacherList/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from  'react'
-
-export default function TeacherList() {
-
-  return <h1>teacher list</h1>
-}
